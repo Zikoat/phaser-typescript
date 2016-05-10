70f05cfb454bf675c328f15cb1bf706df524f04d
Sigurd skole-pc <sschoeler99@gmail.com>
2016-05-10 15:12:57 +0200

    first time on skole-pc. installed some node modules, which i should add to the repo

--- tutorial1.ts
+++ tutorial1.ts
@@ -26,8 +26,18 @@ function create() {
 
 	platforms.enableBody = true;
 
-	let ground = platforms.create(0, game.world.height - 64, "ground");
+	var ground = platforms.create(0, game.world.height - 64, "platform");
 
+	ground.scale.setTo(2, 2);
+
+	ground.body.immovable = true;
+	var ledge = platforms.create(400, 400, 'platform');
+
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150, 250, 'platform');
+
+    ledge.body.immovable = true;
 
 }
 
