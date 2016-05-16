048d54b176d0ca68c5fc5071156b7e4d5a9d1967
Sigurd skole-pc <sschoeler99@gmail.com>
2016-05-16 11:57:46 +0200

    made app.ts into the main game, followed by phaser-typescript tutorial

--- app.ts
+++ app.ts
@@ -1,21 +1,44 @@
 ﻿///<reference path='./node_modules/phaser/typescript/phaser.d.ts' />
-interface Person {
-    firstName: string;
-    lastName: string;
+class MyGame {
+
+	game: Phaser.Game;
+	textValue: Phaser.Text;
+	updateCount: number;
+
+	constructor() {
+		var game = new Phaser.Game(800, 600, Phaser.AUTO, "", { 
+			preload: this.preload, 
+			create: this.create, 
+			update: this.update,
+			render: this.render
+		});
+	}
+
+	preload() {
+		this.game.load.image("sky", "assets/sky.png");
+		this.game.load.image('platform', 'assets/platform.png');
+		this.game.load.image('star', 'assets/star.png');
+		this.game.load.spritesheet("dude", "assets/dude.png", 32, 48);
+	}
+	
+
+	create() {
+		var style = { font: "65px Arial", fill: "#ff0000", align: "center" };
+		this.textValue = this.game.add.text(400, 0, "0", style);
+		this.updateCount = 0;
+	}
+
+	update() {
+		this.textValue.text =  Phaser.Math.fuzzyFloor(this.updateCount++ / 60).toString();
+		this.textValue.y = Phaser.Math.clamp(this.updateCount, 0, 600-65);
+	}
+
+	render() {
+		this.game.debug.text("ey boss", 0, 80);
+	}
+
 }
 
-class Student implements Person {
-    fullName: string;
-    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
-        this.fullName = firstName + " " + middleInitial + " " + lastName;
-    }
+window.onload = () => {
+	var game = new MyGame();
 }
-  
-
-function greeter(person: Person) {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-
-var user = new Student("Zikoort", "v", "goot");
-
-console.log(user.fullName);
